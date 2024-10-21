4135603043fb09566be2fb9f54993ce7bb5acc31
Amisha Kunde <ak4924@ic.ac.uk>
2024-10-21 17:08:38 +0100

    changes

---
+++ .github/workflows/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    input_name = request.form.get("name")
+    input_age = request.form.get("age")
+    return render_template("hello.html", name=input_name, age=input_age)
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/query", methods=["GET"])
+def process_query_route():
+    query = request.args.get("q")
+    return process_query(query)
+
+def process_query(query):
+    if query == "dinosaurs":
+        return "Dinosaurs ruled the Earth 200 million years ago"
+    elif query == "asteroids":
+        return "Unknown"
+    else:
+        return "Unknown"
