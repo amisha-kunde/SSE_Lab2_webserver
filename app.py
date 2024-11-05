138ed4b2b76b0142c020b61e424f2251ee9c6bfb
Amisha Kunde <ak4924@ic.ac.uk>
2024-11-05 15:18:10 +0000

    changes

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ import requests
 
 app = Flask(__name__)
 
+@app.route("/")  # route handler, is a decorator
+def hello_world():  # route endpoint
+    return render_template("index.html")
 
-@app.route("/github")
-def github_username():
-    return render_template("github.html")
+
+@app.route("/query", methods=["GET"])
+def process_query_route():
+    query = request.args.get("q")
+    return process_query(query)
+
 
 
 @app.route("/github-submit", methods=["POST"])
@@ -50,3 +56,27 @@ def process_github_lookup():
     return render_template(
         "githubSubmit.html", username=input_username, repositories=repo_data
     )
+
+def addition_query(query):
+    match = re.search(r"(\d+)\s+plus\s+(\d+)", query)
+    if match:
+        num1 = int(match.group(1))
+        num2 = int(match.group(2))
+        return num1 + num2
+        return str(num1 + num2)
+    else:
+        return "Query not recognized."
+
+
+
+def process_query(query):
+    if "name" in query:
+        return "ak4924"
+    if query == "dinosaurs":
+        return "Dinosaurs ruled the Earth 200 million years ago"
+    if "plus" in query:
+        return addition_query(query)
+    elif query == "asteroids":
+        return "Unknown"
+    else:
+        return "Unknown"
